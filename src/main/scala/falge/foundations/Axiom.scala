8b985d5c79c6a06c89698947c90478ee25f05245
Braulio Valdivielso Martínez <brlvldvlsmrtnz@gmail.com>
2015-07-21 16:29:47 +0200

    Refactored axioms

--- src/main/scala/falge/foundations/Axiom.scala
+++ src/main/scala/falge/foundations/Axiom.scala
@@ -3,22 +3,26 @@ package falge.foundations
 import falge.foundations.statement.Statement
 import falge.util.cartesianPower	
 
-abstract class Axiom[T](val statement : Statement[T]){
+abstract class Axiom[T](){
 	def verify(set: Set[T]) : Boolean
 }
 
-class AxiomForAll[T](val numberOfVariables : Int, statement : Statement[T]) extends Axiom[T](statement) {
+class AxiomExplicit[T]()
+
+class AxiomForAll[T](val numberOfVariables : Int, val statement : Statement[T]) extends Axiom[T]() {
 
 
 	def verify(set : Set[T]) : Boolean = cartesianPower(set, numberOfVariables).forall(statement(_, set))
 
 }
 
-class AxiomExists[T](val numberOfVariables : Int, statement : Statement[T]) extends Axiom[T](statement) {
+class AxiomExists[T](val numberOfVariables : Int, val statement : Statement[T]) extends Axiom[T]() {
 
 	def verify(set : Set[T]) : Boolean = cartesianPower(set, numberOfVariables).exists(statement(_, set))
 
 }
+
+
 
 
 package object axioms {
