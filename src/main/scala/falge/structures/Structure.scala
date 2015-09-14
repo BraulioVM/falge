141d1243c044cb1619fcf35c77d02d7d247df80a
Braulio Valdivielso Martínez <brlvldvlsmrtnz@gmail.com>
2015-09-14 16:57:14 +0200

    Made axioms field in Structure a vector.
    A vector is needed in order to be able to append elements efficiently. The reason why it is necessary for them
    to be appended is that axioms must be verified in order, so that side effects performed by axioms can be
    perceived later by other axioms.

--- src/main/scala/falge/structures/Structure.scala
+++ src/main/scala/falge/structures/Structure.scala
@@ -4,11 +4,11 @@ import falge.foundations.Axiom
 
 class Structure[T](val elements : Set[T]) {
 
-	private var axioms = List[Axiom[T]]();
+	private var axioms = Vector[Axiom[T]]();
 
 	def verify() : Boolean = axioms.forall(_.verify(elements))
 
 	def verifies(axiom : Axiom[T]){
-		axioms = axiom :: axioms
+		axioms = axioms :+ axiom
 	}
 }
